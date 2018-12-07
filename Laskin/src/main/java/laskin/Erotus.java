e278266987acb48ab3d60c646ae5bd5339150544
Sami Lahtinen <thesamsai@gmail.com>
2018-12-07 16:54:02 +0200

    Laskin komentomuodossa

---
+++ Laskin/src/main/java/laskin/Erotus.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author sami
+ */
+public class Erotus implements Komento {
+    
+    TextField tuloste;
+    TextField syote;
+    Button nollaa;
+    Button undo;
+    Sovelluslogiikka sovellus;
+    int arvo = 0;
+    
+    public Erotus(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloste = tuloskentta;
+        this.syote = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    @Override
+    public void suorita() {
+        try {
+            this.arvo = Integer.parseInt(syote.getText());
+            sovellus.miinus(arvo);
+            
+            syote.setText("");
+            tuloste.setText("" + sovellus.tulos());
+            
+            nollaa.disableProperty().set(false);
+        } catch (Exception e) {
+            
+        }
+    }
+
+    @Override
+    public void peru() {
+    }
+    
+}
---
+++ Laskin/src/main/java/laskin/Nollaa.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author sami
+ */
+public class Nollaa implements Komento {
+    
+    TextField tuloste;
+    TextField syote;
+    Button nollaa;
+    Button undo;
+    Sovelluslogiikka sovellus;
+    int arvo = 0;
+    
+    public Nollaa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloste = tuloskentta;
+        this.syote = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    @Override
+    public void suorita() {
+        this.arvo = sovellus.tulos();
+        sovellus.nollaa();
+            
+        syote.setText("");
+        tuloste.setText("" + sovellus.tulos());
+        
+        nollaa.disableProperty().set(true);
+    }
+
+    @Override
+    public void peru() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
